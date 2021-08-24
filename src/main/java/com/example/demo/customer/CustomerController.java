e30dc49c7ca7d8661af764ce74c752e6061a796d
hingsmy <44699571+hingsmy@users.noreply.github.com>
2021-08-24 14:21:44 +0800

    Dependency injection

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,13 +1,21 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping
     Customer getCustomer() {
-        return new Customer(1L, "James Bond");
+        return customerService.getCustomer();
     }
 }
--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,4 +1,11 @@
 package com.example.demo.customer;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerService {
+
+    Customer getCustomer() {
+        return new Customer(1L, "James Bond");
+    }
 }
