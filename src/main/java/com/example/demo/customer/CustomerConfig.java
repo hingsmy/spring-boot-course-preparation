cd5fbeaeb997da2fb6b4c05e8eae5146625a4133
hingsmy <hingsmy@gmail.com>
2021-09-19 18:46:32 +0800

    @Value and environment

--- src/main/java/com/example/demo/customer/CustomerConfig.java
+++ src/main/java/com/example/demo/customer/CustomerConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfig {
@@ -11,12 +13,21 @@ public class CustomerConfig {
     @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
     @Bean
     CommandLineRunner commandLineRunner() {
 
         return args ->
+        {
             System.out.println("Command line runner hooray!");
-
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.version"));
+        };
     }
 
     @Bean
